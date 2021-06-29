be52bbf9453427ec9cd4df4a096c4b3a2f52d77f
quabid <quabidlord@gmail.com>
2021-06-29 13:52:21 -0400

    Modified comments

--- app.py
+++ app.py
@@ -36,10 +36,17 @@ commandKey: Sends a system command to the target host computer system, then outp
     @params command:            The command to send to the system
     @params port:               The port to connect with; defaults to 30777
 
-tree: Sends the tree command to the target host computer system, then outputs the results to standard out.
+treePass: Sends the tree command to the target host computer system, then outputs the results to standard out.
     @params ip:                 The hostname or IP address of the target system
     @params username:           A username from the system's accounts
     @params password:           The user's password
+    @params port:               The port to make the connection
+
+treeKey: Sends the tree command to the target host computer system, then outputs the results to standard out.
+    @params ip:                 The hostname or IP address of the target system
+    @params username:           A username from the system's accounts
+    @params key_file_path:      The absolute path to the private key file to access the host
+    @params pass_phrase         The passphrase for the private key
     @params port:               The port to make the connection
 """
 
